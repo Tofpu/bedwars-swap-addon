dc1ebc56f9311ec7267b8c9a00b16a6c624c9c62
Tofpu <47629321+Tofpu@users.noreply.github.com>
2022-04-25 06:57:43 +0200

    bug fixes & made the swap tasks to happen for all team members

--- src/main/java/io/tofpu/bedwarsswapaddon/model/swap/pool/task/sub/impl/InventorySwapTask.java
+++ src/main/java/io/tofpu/bedwarsswapaddon/model/swap/pool/task/sub/impl/InventorySwapTask.java
@@ -1,55 +1,89 @@
 package io.tofpu.bedwarsswapaddon.model.swap.pool.task.sub.impl;
 
+import com.andrei1058.bedwars.api.arena.team.ITeam;
 import io.tofpu.bedwarsswapaddon.model.swap.pool.task.sub.SubTask;
 import org.bukkit.Color;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.LeatherArmorMeta;
 
+import java.util.List;
+
 public class InventorySwapTask implements SubTask {
     @Override
     public void run(final SubTaskContext context) {
-        final PlayerInventory playerOneInventory = context.getPlayerOne()
-                .getInventory();
-        final PlayerInventory playerTwoInventory = context.getPlayerTwo()
-                .getInventory();
+        final ITeam playerOneTeam = context.getPlayerOneTeam();
+        final ITeam playerTwoTeam = context.getPlayerTwoTeam();
 
-        swapArmorContents(playerOneInventory, playerTwoInventory);
+        final List<Player> teamOneMembers = playerOneTeam
+                .getMembers();
+        final List<Player> teamTwoMembers = playerTwoTeam
+                .getMembers();
+
+        teamOneMembers.forEach(member -> {
+            swapArmorContentsColor(member.getInventory(),
+                    playerTwoTeam.getColor().bukkitColor());
+        });
+
+        teamTwoMembers.forEach(member -> {
+            swapArmorContentsColor(member.getInventory(),
+                    playerOneTeam.getColor().bukkitColor());
+        });
     }
 
-    private void swapArmorContents(final PlayerInventory playerOneInventory, final PlayerInventory playerTwoInventory) {
-        final ItemStack[] firstPlayerArmorContents =
-                playerOneInventory.getArmorContents();
-        final ItemStack[] secondPlayerArmorContents = playerTwoInventory.getArmorContents();
+    private void swapArmorContentsColor(final PlayerInventory playerInventory,
+            final Color color) {
+        final ItemStack[] armorContents = playerInventory.getArmorContents();
 
         for (int i = 0; i < 4; i++) {
-            final ItemStack firstPlayerArmorContent = firstPlayerArmorContents[i];
-            final ItemStack secondPlayerArmorContent = secondPlayerArmorContents[i];
+            final ItemStack armorContent = armorContents[i];
 
-            final boolean isBothItemValid = isItemValid(firstPlayerArmorContent) && isItemValid(secondPlayerArmorContent);
-
-            if (!isBothItemValid || !"LEATHER".contains(firstPlayerArmorContent.getType().name())) {
+            if (!isItemValid(armorContent) || !armorContent.getType().name()
+                    .contains("LEATHER")) {
                 continue;
             }
 
-            swapItemColor(firstPlayerArmorContent, secondPlayerArmorContent);
+            final LeatherArmorMeta armorMeta = (LeatherArmorMeta) armorContent.getItemMeta();
+            armorMeta.setColor(color);
+            armorContent.setItemMeta(armorMeta);
         }
     }
 
-    private void swapItemColor(final ItemStack firstPlayerItem, final ItemStack secondPlayerItem) {
-        final LeatherArmorMeta firstPlayerArmorMeta = (LeatherArmorMeta) firstPlayerItem.getItemMeta();
-        final LeatherArmorMeta secondPlayerArmorMeta = (LeatherArmorMeta) secondPlayerItem.getItemMeta();
-
-        final Color firstPlayerArmorColor =
-                Color.fromBGR(firstPlayerArmorMeta.getColor().asBGR());
-
-        firstPlayerArmorMeta.setColor(secondPlayerArmorMeta.getColor());
-        secondPlayerArmorMeta.setColor(firstPlayerArmorColor);
-
-        firstPlayerItem.setItemMeta(firstPlayerArmorMeta);
-        secondPlayerItem.setItemMeta(secondPlayerArmorMeta);
-    }
+//    private void swapArmorContents(final PlayerInventory playerOneInventory, final PlayerInventory playerTwoInventory) {
+//        final ItemStack[] firstPlayerArmorContents =
+//                playerOneInventory.getArmorContents();
+//        final ItemStack[] secondPlayerArmorContents = playerTwoInventory.getArmorContents();
+//
+//        for (int i = 0; i < 4; i++) {
+//            final ItemStack firstPlayerArmorContent = firstPlayerArmorContents[i];
+//            final ItemStack secondPlayerArmorContent = secondPlayerArmorContents[i];
+//
+//            final boolean isBothItemValid = isItemValid(firstPlayerArmorContent) && isItemValid(secondPlayerArmorContent);
+//
+//            if (!isBothItemValid || !firstPlayerArmorContent.getType().name()
+//                    .contains("LEATHER")) {
+//                continue;
+//            }
+//
+//            swapItemColor(firstPlayerArmorContent, secondPlayerArmorContent);
+//        }
+//    }
+//
+//    private void swapItemColor(final ItemStack firstPlayerItem, final ItemStack secondPlayerItem) {
+//        final LeatherArmorMeta firstPlayerArmorMeta = (LeatherArmorMeta) firstPlayerItem.getItemMeta();
+//        final LeatherArmorMeta secondPlayerArmorMeta = (LeatherArmorMeta) secondPlayerItem.getItemMeta();
+//
+//        final Color firstPlayerArmorColor =
+//                Color.fromBGR(firstPlayerArmorMeta.getColor().asBGR());
+//
+//        firstPlayerArmorMeta.setColor(secondPlayerArmorMeta.getColor());
+//        secondPlayerArmorMeta.setColor(firstPlayerArmorColor);
+//
+//        firstPlayerItem.setItemMeta(firstPlayerArmorMeta);
+//        secondPlayerItem.setItemMeta(secondPlayerArmorMeta);
+//    }
 
     private boolean isItemValid(final ItemStack itemStack) {
         return itemStack != null && itemStack.getType() != Material.AIR;
--- src/main/java/io/tofpu/bedwarsswapaddon/model/swap/pool/task/sub/impl/LocationSwapTask.java
+++ src/main/java/io/tofpu/bedwarsswapaddon/model/swap/pool/task/sub/impl/LocationSwapTask.java
@@ -4,15 +4,39 @@ import io.tofpu.bedwarsswapaddon.model.swap.pool.task.sub.SubTask;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
+import java.util.List;
+
 public class LocationSwapTask implements SubTask {
     @Override
     public void run(final SubTaskContext context) {
-        final Player playerOne = context.getPlayerOne();
-        final Player playerTwo = context.getPlayerTwo();
+        final List<Player> teamOneMembers = context.getPlayerOneTeam()
+                .getMembers();
+        final List<Player> teamTwoMembers = context.getPlayerTwoTeam()
+                .getMembers();
+        for (int i = 0; i < Math.max(teamOneMembers.size(), teamTwoMembers.size()); i++) {
+            final Player teamOneMember = getSafety(teamOneMembers, i);
+            final Player teamTwoMember = getSafety(teamTwoMembers, i);
 
-        final Location playerOneLocation = playerOne.getLocation().clone();
+            if (teamOneMember == null && teamTwoMember != null) {
+                teamTwoMember.teleport(context.getPlayerOneTeam().getSpawn());
+                continue;
+            }
 
-        playerOne.teleport(playerTwo.getLocation());
-        playerTwo.teleport(playerOneLocation);
+            if (teamOneMember != null && teamTwoMember == null) {
+                teamOneMember.teleport(context.getPlayerTwoTeam().getSpawn());
+                continue;
+            }
+
+            final Location firstMemberLocation = teamOneMember.getLocation().clone();
+            teamOneMember.teleport(teamTwoMember.getLocation());
+            teamTwoMember.teleport(firstMemberLocation);
+        }
+    }
+
+    private <T> T getSafety(final List<T> list, final int index) {
+        if (index < 0 || index >= list.size()) {
+            return null;
+        }
+        return list.get(index);
     }
 }
