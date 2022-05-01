3dd8aa058caf202b4eab0e11b9e6571dfc2027fd
Tofpu <47629321+Tofpu@users.noreply.github.com>
2022-05-01 11:25:46 +0200

    started swapping team traps

--- src/main/java/io/tofpu/bedwarsswapaddon/model/swap/pool/task/sub/impl/TeamSwapTask.java
+++ src/main/java/io/tofpu/bedwarsswapaddon/model/swap/pool/task/sub/impl/TeamSwapTask.java
@@ -1,16 +1,14 @@
 package io.tofpu.bedwarsswapaddon.model.swap.pool.task.sub.impl;
 
-import com.andrei1058.bedwars.BedWars;
 import com.andrei1058.bedwars.api.arena.team.ITeam;
+import com.andrei1058.bedwars.api.upgrades.EnemyBaseEnterTrap;
 import com.andrei1058.bedwars.sidebar.BedWarsScoreboard;
 import io.tofpu.bedwarsswapaddon.model.swap.pool.task.sub.SubTask;
 import org.bukkit.entity.Player;
-import org.bukkit.scoreboard.Scoreboard;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class TeamSwapTask implements SubTask {
     @Override
@@ -59,6 +57,7 @@ public class TeamSwapTask implements SubTask {
     }
 
     private void switchTeamTiers(final ITeam playerOneTeam, final ITeam playerTwoTeam) {
+        // swaps the team upgrades
         final ConcurrentHashMap<String, Integer> firstTeamUpgradeTiers = new ConcurrentHashMap<>(playerOneTeam.getTeamUpgradeTiers());
 
         playerOneTeam.getTeamUpgradeTiers()
@@ -70,5 +69,17 @@ public class TeamSwapTask implements SubTask {
                 .clear();
         playerTwoTeam.getTeamUpgradeTiers()
                 .putAll(firstTeamUpgradeTiers);
+
+        // swaps the active traps
+        final LinkedList<EnemyBaseEnterTrap> firstTeamActiveTraps = new LinkedList<>(playerOneTeam.getActiveTraps());
+        playerOneTeam.getActiveTraps()
+                .clear();
+        playerOneTeam.getActiveTraps()
+                .addAll(playerTwoTeam.getActiveTraps());
+
+        playerTwoTeam.getActiveTraps()
+                .clear();
+        playerTwoTeam.getActiveTraps()
+                .addAll(firstTeamActiveTraps);
     }
 }
