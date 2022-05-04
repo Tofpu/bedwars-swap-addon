e008b9f8fed84ef6bf75cd9a789b721e17934f52
Tofpu <47629321+Tofpu@users.noreply.github.com>
2022-05-04 08:01:02 +0200

    swap algorithm overhaul; forgot to include the code

--- src/main/java/io/tofpu/bedwarsswapaddon/model/swap/pool/SwapPoolHandlerGame.java
+++ src/main/java/io/tofpu/bedwarsswapaddon/model/swap/pool/SwapPoolHandlerGame.java
@@ -14,7 +14,6 @@ import org.bukkit.scheduler.BukkitTask;
 
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
 
--- src/main/java/io/tofpu/bedwarsswapaddon/model/swap/pool/task/SwapPoolTaskGame.java
+++ src/main/java/io/tofpu/bedwarsswapaddon/model/swap/pool/task/SwapPoolTaskGame.java
@@ -11,7 +11,9 @@ import io.tofpu.bedwarsswapaddon.model.wrapper.TeamSnapshot;
 import io.tofpu.bedwarsswapaddon.util.TeamUtil;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class SwapPoolTaskGame extends SwapPoolTaskBase {
@@ -22,7 +24,7 @@ public class SwapPoolTaskGame extends SwapPoolTaskBase {
             "  Members: %s : %s";
 
     private static final String FOUND_TEAM_DEBUG =
-            "\n" + "Iteration Item:\n " + " Team: %s\n" + "  Size: %s : %s\n" +
+            "\n" + "Iteration Item[%s]:\n " + " Team: %s\n" + "  Size: %s : %s\n" +
             "  Members: %s : %s";
 
     private static final String NEXT_FOUND_TEAM_DEBUG =
@@ -47,61 +49,49 @@ public class SwapPoolTaskGame extends SwapPoolTaskBase {
                 .collect(Collectors.toList());
 
         LogHandler.get()
-                .debug("Filtered teams: " + filteredTeams);
+                .debug("Filtered teams: " + filteredTeams.stream().map(TeamSnapshot::getName)
+                                .collect(Collectors.joining(", ")));
 
 
-        int index = 1;
-        for (final TeamSnapshot team : filteredTeams) {
+        final Map<TeamSnapshot, TeamSnapshot> swapMap = new HashMap<>();
 
-            LogHandler.get()
-                    .debug(String.format(FOUND_TEAM_DEBUG, team.getColor(),
-                            team.getSize(),
-                            team.getLiveMembers().size(),
-                            TeamUtil.toString(team.getLiveMembers()),
-                            TeamUtil.toString(team.getLiveMembers())));
+        for (int i = 0; i < filteredTeams.size(); i++) {
+            final TeamSnapshot team = filteredTeams.get(i);
 
-            final TeamSnapshot nextTeam;
-
-            if ((index) == filteredTeams.size()) {
-                nextTeam = filteredTeams.get(0);
-
-                if (nextTeam.getColor() == team.getColor()) {
-                    LogHandler.get()
-                            .debug("Nothing to swap with. Exiting.");
-                    break;
-                }
-            } else {
-                nextTeam = filteredTeams.get(index);
+            if (i == filteredTeams.size() - 1) {
+                swapMap.put(team, filteredTeams.get(0));
             }
 
-            if (nextTeam == null) {
-                LogHandler.get()
-                        .debug("No more teams to swap. Exiting.");
+            for (int j = i + 1; j < filteredTeams.size(); j++) {
+                final TeamSnapshot nextTeam = filteredTeams.get(j);
+
+                if (swapMap.containsValue(nextTeam) || nextTeam.getColor() == team.getColor() || nextTeam.isBedDestroyed() != team.isBedDestroyed()) {
+                    continue;
+                }
+
+                swapMap.put(team, nextTeam);
                 break;
             }
+        }
 
-            LogHandler.get()
-                    .debug(String.format(NEXT_FOUND_TEAM_DEBUG, team.getColor(),
-                            team.getSize(),
-                            team.getLiveMembers().size(),
-                            TeamUtil.toString(team.getLiveMembers()),
-                            TeamUtil.toString(team.getLiveMembers())));
-
-            LogHandler.get().debug(String.format(SWAPPING_WITH_DEBUG, team.getColor(), nextTeam.getColor()));
+        for (final Map.Entry<TeamSnapshot, TeamSnapshot> entry : swapMap.entrySet()) {
+            final TeamSnapshot from = entry.getKey();
+            final TeamSnapshot to = entry.getValue();
+            LogHandler.get().debug(String.format(SWAPPING_WITH_DEBUG, from.getColor(), to.getColor()));
 
             final MessageHolder messageHolder = MessageHolder.get();
-            TeamUtil.broadcastMessageTo(messageHolder.swapMessageAnnouncement.replace("%team%", TeamUtil.teamOf(nextTeam.getColor())), team);
-            TeamUtil.broadcastTitleTo(messageHolder.swapTitleAnnouncement.replace("%team%", TeamUtil.teamOf(nextTeam.getColor())), team);
+            TeamUtil.broadcastMessageTo(messageHolder.swapMessageAnnouncement.replace(
+                    "%team%", TeamUtil.teamOf(to.getColor())), from);
+            TeamUtil.broadcastTitleTo(messageHolder.swapTitleAnnouncement.replace(
+                    "%team%", TeamUtil.teamOf(to.getColor())), from);
 
-            subTasksList().forEach(subTask -> subTask.run(new SubTask.SubTaskContext(subTask, arena, team, nextTeam)));
-
+            subTasksList().forEach(subTask -> subTask.run(new SubTask.SubTaskContext(subTask, arena, from, to)));
             LogHandler.get().debug(String.format(AFTER_FORMAT_DEBUG,
-                    team.getColor(), team.getMembers().size(), team.getLiveMembers().size(),
-                    TeamUtil.toString(team.getMembers()), TeamUtil.toString(team.getLiveMembers()),
-                    nextTeam.getColor(), nextTeam.getMembers().size(), nextTeam.getLiveMembers().size(),
-                    TeamUtil.toString(nextTeam.getMembers()), TeamUtil.toString(nextTeam.getLiveMembers())));
-
-            index++;
+                    from.getColor(), from.getMembers().size(),
+                    from.getLiveMembers().size(),
+                    TeamUtil.toString(from.getMembers()), TeamUtil.toString(from.getLiveMembers()),
+                    to.getColor(), to.getMembers().size(), to.getLiveMembers().size(),
+                    TeamUtil.toString(to.getMembers()), TeamUtil.toString(to.getLiveMembers())));
         }
     }
 
