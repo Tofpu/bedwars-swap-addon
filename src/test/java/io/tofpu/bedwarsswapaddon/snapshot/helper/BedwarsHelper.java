2b0f827c33701680148ee72da0df95b3726987c7
Tofpu <tofpuu@gmail.com>
2023-06-06 06:11:57 +0300

    Overhaul of swappage algorithm

--- src/test/java/io/tofpu/bedwarsswapaddon/snapshot/helper/BedwarsHelper.java
+++ src/test/java/io/tofpu/bedwarsswapaddon/snapshot/helper/BedwarsHelper.java
@@ -1,14 +1,28 @@
 package io.tofpu.bedwarsswapaddon.snapshot.helper;
 
 import com.andrei1058.bedwars.api.arena.team.ITeam;
+import com.andrei1058.bedwars.api.arena.team.TeamColor;
 import io.tofpu.bedwarsswapaddon.snapshot.LiveObject;
+import io.tofpu.bedwarsswapaddon.snapshot.util.BedwarsMocker;
 import io.tofpu.bedwarsswapaddon.wrapper.snapshot.TeamSnapshot;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class BedwarsHelper extends BukkitHelper {
     @NotNull
     protected TeamSnapshot snapshot(ITeam redTeam) {
         return new TeamSnapshot(redTeam);
+    }
+
+    protected TeamSnapshot snapshot(TeamColor color, boolean bedDestroyed) {
+        return snapshot(BedwarsMocker.bedWarsTeam(color, bedDestroyed));
+    }
+
+    protected List<TeamSnapshot> teams(TeamSnapshot... snapshots) {
+        return Arrays.stream(snapshots).collect(Collectors.toList());
     }
 
     protected LiveObject live(ITeam team) {
