7bfad45bca9d1aae1fa8a92c4a01c058c3ac0a6b
Tofpu <tofpuu@gmail.com>
2023-09-16 14:05:56 +0300

    Use team's name instead of their color
    - Polish up code
    - Include team color when mentioning team name
    - Show team's name instead of the team's color

--- src/main/java/io/tofpu/bedwarsswapaddon/util/ColorUtil.java
+++ src/main/java/io/tofpu/bedwarsswapaddon/util/ColorUtil.java
@@ -6,11 +6,19 @@ import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 
 public class ColorUtil {
-    public static Component translate(final String message) {
+    public static Component deserializeMiniMessage(final String message) {
         return MiniMessage.miniMessage().deserialize(message);
     }
 
-    public static TextComponent translateLegacy(final String message) {
+    public static String serializeMiniMessage(final Component component) {
+        return MiniMessage.miniMessage().serialize(component);
+    }
+
+    public static String serializeToLegacy(final Component component) {
+        return BukkitComponentSerializer.legacy().serialize(component);
+    }
+
+    public static TextComponent legacyToComponent(final String message) {
         return BukkitComponentSerializer.legacy().deserialize(message);
     }
 }
